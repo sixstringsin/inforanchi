a6979d0a2661fac96856d86d0b4c549cd80b7959
sixstringsin <connect2nis@gmail.com>
2021-05-02 09:56:10 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -207,31 +207,37 @@
   }
   // Porfolio isotope and filter
   $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
+    if($(".portfolio-container") && $(".portfolio-container").length>0){
+      var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item'
       });
-    });
 
+      $('#portfolio-flters li').on('click', function() {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+      }
+    });
+  
     // Initiate venobox (lightbox feature used in portofilo)
     $(document).ready(function() {
+    if($(".venobox") && $(".venobox").length>0){
       $('.venobox').venobox();
+    }
     });
   });
 
   // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+  if($(".portfolio-details-carousel") && $(".portfolio-details-carousel").length>0){
+    $(".portfolio-details-carousel").owlCarousel({
+      autoplay: true,
+      dots: true,
+      loop: true,
+      items: 1
+    });
+  }
 
 })(jQuery);
