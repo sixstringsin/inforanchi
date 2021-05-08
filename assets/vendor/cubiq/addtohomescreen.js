9cb309512d00066caa804b7ab12aab3a37c8f03f
sixstringsin <connect2nis@gmail.com>
2021-05-08 14:05:25 -0400

    Update addtohomescreen.js

--- assets/vendor/cubiq/addtohomescreen.js
+++ assets/vendor/cubiq/addtohomescreen.js
@@ -52,8 +52,8 @@ ath.defaults = {
 	mandatory: false,			// you can't proceed if you don't add the app to the homescreen
 	autostart: true,			// show the message automatically
 	skipFirstVisit: false,		// show only to returning visitors (ie: skip the first time you visit)
-	startDelay: 2,				// display the message after that many seconds from page load
-	lifespan: 10,				// life of the message in seconds
+	startDelay: 5,				// display the message after that many seconds from page load
+	lifespan: 15,				// life of the message in seconds
 	displayPace: 1440,			// minutes before the message is shown again (0: display every time, default 24 hours) default 1440
 	maxDisplayCount: 0,			// absolute maximum number of times the message will be shown to the user (0: no limit)
 	icon: true,					// add touch icon to the message
