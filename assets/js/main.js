ed3a9037c7df72e4d823bfb9842b407df584cf7f
sixstringsin <connect2nis@gmail.com>
2021-05-10 22:14:18 -0400

    last try

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,10 +34,11 @@ function sendcontacttoga(){
 /*end ga*/
 
 /*Show Disclaimer*/
-//function modaloff(){
-//$('#dynamicModal').modal('hide');	
+function modaloff(){
+$('#dynamicModal').modal('hide');
+return true;	
 //$('#dynamicModal').off('shown.bs.modal');
-//}
+}
 function redirectExternal(url){
  sendtoga('outbound',url,'');
  return true;
@@ -56,7 +57,7 @@ function showModal(context,name,url) {
       '</div>'+
       '<div class="modal-footer">'+
 	'<a class="btn btn-link" data-dismiss="modal" style="color:gray;font-size:12px">Stay on this page</a>&nbsp;'+
-        '<a href='+url+' target="_blank" class="btn btn-secondary" onClick="$('#dynamicModal').modal('hide');redirectExternal('+url+');">Continue</a>'+
+        '<a href='+url+' target="_blank" class="btn btn-secondary" onClick="modaloff();redirectExternal('+url+');">Continue</a>'+
       '</div>';
   }else{
 	modalHtml='<div class="modal-body">'+
